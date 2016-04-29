9e8e6af70f025ad354478a93a8acd65cd705f60f
ealvarezanaya <ealvarez.anaya@gmail.com>
2016-04-29 05:04:52 +0000

    nuevo_cambio

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,20 +1,31 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope,$ionicPopup,$timeout,$cordovaSQLite) {
-    $scope.showAlert=function(){
-        var alertPopup=$ionicPopup.alert({
-            title:'Agenda',
-            template:'Datos Guardados'
-        });
+.controller('DashCtrl', function($scope,$ionicPopup, $timeout,$cordovaSQLite) {
+    
+    $scope.showAlert = function() {
+     var alertPopup = $ionicPopup.alert({
+       title: 'Agenda',
+       template: 'Datos guardados'
+     });
     }
-    $scope.guardar=function(persona){
-        $cordovaSQLite.execute(db,'INSERT INTO agenda(nombre,apellido,telefono,email)VALUES(?,?,?,?)',[persona.nombre,persona.apellido,persona.telefono,persona,email]).then(function(result)
-        {
-            $scope.statusMessage="Registro Guardado!";
-            }, function(error){
-                $scope.statusMessage="Error al guardar:"+error.message;
-            })
+    
+    $scope.guardar = function(persona){
+        
+        $cordovaSQLite.execute(db, 'INSERT INTO agenda (nombre,apellido,telefono,email) VALUES (?,?,?,?)', [persona.nombre,persona.apellido,persona.telefono,persona.email])
+        .then(function(result) {
+            $scope.statusMessage = "Registro guardado!";
+        }, function(error) {
+            $scope.statusMessage = "Error al guardar: " + error.message;
+        })
+        
+        /*
+        console.log("Nombre: "+persona.nombre);
+        console.log("Apellido: "+persona.apellido);
+        console.log("Telefono: "+persona.telefono);
+        console.log("Email: "+persona.email);
+        */
     }
+    
 })
 
 .controller('ChatsCtrl', function($scope, Chats,$cordovaSQLite) {
@@ -27,8 +38,31 @@ angular.module('starter.controllers', [])
   //});
 
   $scope.chats = Chats.all();
-  $scope.getAll=function(){
-      $scope.chats=chats.all();
+  //$scope.chats = [];
+  
+  /*
+  $cordovaSQLite.execute(db, 'SELECT * FROM agenda ORDER BY id DESC')
+       .then(
+          function(result) {
+             if (result.rows.length > 0) {
+                      for(var i = 0; i < result.rows.length; i++)
+                      { 
+                        $scope.chats.push({"nombre":result.rows.item(i).nombre,
+                                    "apellido":result.rows.item(i).apellido,
+                                    "telefono":result.rows.item(i).telefono,
+                                    "email":result.rows.item(i).email});
+                      }
+                    }
+                },
+                function(error) {
+                    $scope.statusMessage = "Error on loading: " + error.message;
+                }
+        );
+  */
+
+  $scope.getAll = function()
+  {
+      $scope.chats = Chats.all();
   };
   
   $scope.remove = function(chat) {
@@ -37,18 +71,25 @@ angular.module('starter.controllers', [])
 })
 
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats,$cordovaSQLite) {
+  
+  //$scope.chat = Chats.get($stateParams.chatId);
   $scope.persona = Chats.get($stateParams.chatId);
-  scope.guardar=function(persona){
-      $cordovaSQLite.execute(db,'UPDATE agenda set nombre=?,apellido=?,telefono=?,email=? where id=?',
-      [persona.nombre,persona.apellido,persona.telefono,persona.email,persona.id])
-      .then (function(result) {
-        $scope.statusMessage="Registro Guardado!";  
-      },function(error){
-          $scope.statusMessage="Error al guardar:"+ error.message;
-      })
-      console.log("NOMBRE:" + persona.nombre);
-      console.log("ID:" + persona.id);
-  }
+  
+  $scope.guardar = function(persona){
+        
+        $cordovaSQLite.execute(db, 'UPDATE agenda set nombre=?,apellido=?,telefono=?,email=? where id = ?', [persona.nombre,persona.apellido,persona.telefono,persona.email,persona.id])
+        .then(function(result) {
+            $scope.statusMessage = "Registro guardado!";
+        }, function(error) {
+            $scope.statusMessage = "Error al guardar: " + error.message;
+        })
+        
+        console.log("NOMBRE: "+persona.nombre);
+        console.log("ID: "+persona.id);
+    }
+  
+    
+  
 })
 
 .controller('AccountCtrl', function($scope) {
